d7c6cb7cda40b9315987831123a8d731d5cf4b20
nkathamilcahmakini <nkathamilcah@gmail.com>
2023-02-03 10:53:48 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+
+    var fullName = arrayOf("Favour Makena", "Sally Moraa", "Samuel Kimathi")
+    println(fullName)
+
+    var age = arrayOf(24, 21,19)
+    println(age)
+
+    var weight: Array<Int> = arrayOf(65,58,72)
+    println(weight)
+
+    var phoneNumber: Array<Long> = arrayOf(+254723460780, +256745670980, +255712640790)
+    println(phoneNumber)
+
+    var citizens: Array<Boolean> = arrayOf(true, false, false)
+    println(citizens)
+
+
+
+
+}
